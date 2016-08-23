39e72a30638d205078f80fef45a761b7d48dc635
bjo2008cn <18600882246@163.com>
2016-08-23 16:36:27 +0800

    refactor

--- common/src/main/java/com/yeahmobi/yscheduler/common/fileserver/FileServer.java
+++ common/src/main/java/com/yeahmobi/yscheduler/common/fileserver/FileServer.java
@@ -11,9 +11,9 @@ public interface FileServer {
      * 往特定文件写数据，如果文件不存在，则创建
      *
      * @param nameSpace 命名空间，因为fileServer会负责各种业务场景的文件，nameSpace用于标识业务场景，对应场景的文件存放方式由fileServer决定
-     * @param fileName 文件名(不带任何路径)
-     * @param data append的数据
-     * @param append 是否追加
+     * @param fileName  文件名(不带任何路径)
+     * @param data      append的数据
+     * @param append    是否追加
      */
     public void write(String nameSpace, String fileName, byte[] data, boolean append) throws FileServerException;
 
