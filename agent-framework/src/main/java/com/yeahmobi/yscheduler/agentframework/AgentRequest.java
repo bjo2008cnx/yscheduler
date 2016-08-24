d9aeb9703242a9ab776fb7c8af8444de6f878f51
bjo2008cn <18600882246@163.com>
2016-08-24 14:33:38 +0800

    refactor

--- agent-framework/src/main/java/com/yeahmobi/yscheduler/agentframework/AgentRequest.java
+++ agent-framework/src/main/java/com/yeahmobi/yscheduler/agentframework/AgentRequest.java
@@ -3,18 +3,21 @@
  */
 package com.yeahmobi.yscheduler.agentframework;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-
 /**
  * @author Leo.Liang
  */
+@Data
+@AllArgsConstructor
 public class AgentRequest {
 
     public static final String  REQKEY_EVENT_TYPE = "eventType";
@@ -22,25 +25,6 @@ public class AgentRequest {
 
     private String              eventType;
     private Map<String, String> params;
-
-    public AgentRequest(String eventType, Map<String, String> params) {
-        this.eventType = eventType;
-        this.params = params;
-    }
-
-    /**
-     * @return the eventType
-     */
-    public String getEventType() {
-        return this.eventType;
-    }
-
-    /**
-     * @return the params
-     */
-    public Map<String, String> getParams() {
-        return this.params;
-    }
 
     @SuppressWarnings("unchecked")
     public static AgentRequest valueOf(HttpServletRequest req) {
